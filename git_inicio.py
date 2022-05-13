1b4730f10d4e7e2235ef2eedab978600f1bb5aae
renato <renvillalba99@hotmail.com>
2022-05-12 23:33:53 -0300

    Modificado

--- git_inicio.py
+++ git_inicio.py
@@ -1,43 +1,10 @@
-from random import choice
-from utiles import *
 
+print("Empezando con git y github")
 
-def adivinar(): #elige una palabra al azar
-    LISTA = ["abran", "besos", "espia", "gatos", "manda"]
-    palabra = choice(LISTA)
-    return palabra
+def mostrando_mensaje():
 
-def palabra_arriesgo (): #Recibe una palabra al azar y devuelve un booleano si se adivina mientras sea de len 5 
-    PALABRA = adivinar()
-    adivinar_1 = print("Palabra a adivinar: ?????")
-    arriesgo = input("Arriesgo: ")
-    ganaste = False
-    while len(arriesgo) != 5:
-        print ("El largo de la palabra debe contener 5 letras\n")
-        arriesgo = input("Arriesgo: \n")
+    saludo = print("Vamos paso a paso logrando el objetivito")
 
-    for i in range (0, len(arriesgo)):
+    return saludo
 
-        if i in range (0, len (PALABRA)):
-
-            if arriesgo[i] == PALABRA[i]:
-                print (obtener_color("Verde") + arriesgo[i], end = "")
-
-            elif (arriesgo[i] in PALABRA) and arriesgo[i] != PALABRA[i]:
-                print (obtener_color("Amarillo") + arriesgo[i], end = "")
-
-            else:
-                print (obtener_color("Defecto") + arriesgo[i], end = "")
-
-    if arriesgo == PALABRA:
-        adivinar_1 = print(obtener_color("Defecto") + "\nPalabra a adivinar: ", PALABRA)
-        print("Ganaste!")
-        #ganaste = True 
-    else:
-        adivinar_1 = print(obtener_color("Defecto") + "\nPalabra a adivinar: ", PALABRA)
-        print("Perdiste!")
-        #ganaste = False
-
-    return ganaste
-
-palabra_arriesgo ()
+mostrando_mensaje()
