4410d074b3bc3e2781a3fcd4f37979570d48ec27
renato <renvillalba99@hotmail.com>
2022-05-12 23:56:43 -0300

    Saludo1

---
+++ branchs.py
@@ -0,0 +1,13 @@
+'''
+Vamos a usar un poco de ramas para este programa
+'''
+
+def saludo_inicial():
+
+    saludo1 = print("Buenas!")
+    saludo2 = print("Tas bien?")
+
+    return saludo1 + saludo2
+
+saludo_inicial()
+
